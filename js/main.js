14409e713fa49a868506467221843059331eee44
Eilon <eilon@securingsam.com>
2018-04-18 19:02:57 +0300

    First commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#send").click(function(){
-        $.post("http://insurance.ibitcoin.co.il/register/", {
+        $.ajax("http://insurance.ibitcoin.co.il/register/", {
             first_name: $("#first_name").value,
         },
         function(data, status){
